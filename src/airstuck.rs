db14d9ee2221fdd847fb7a061806a88d5163486d
tsuji <tsujianii@tuta.io>
2021-08-28 22:47:25 +0800

    dont need that

--- src/airstuck.rs
+++ src/airstuck.rs
@@ -5,13 +5,6 @@ use crate::input::{Button, Input};
 pub const TWO24: i32 = 2_i32.pow(24);
 
 pub fn create_move<'a>(command: &'a mut Command, input: &'a Input, button: Button) {
-    let mut logger = Logger::new();
-
-    writeln!(logger, "airstuck!");
-    writeln!(logger, "command = {:?}", command);
-    writeln!(logger, "input = {:?}", input);
-    writeln!(logger, "button = {:?}", button);
-
     if command.in_attack() || command.in_attack2() {
         return;
     }
